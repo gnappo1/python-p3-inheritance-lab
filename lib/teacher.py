4833ecbaf3896c58642b2ac38bb746b1b98078a3
Matteo Piccini <gnappo.1@live.it>
2022-12-30 17:31:59 -0800

    random refactor

--- lib/teacher.py
+++ lib/teacher.py
@@ -8,8 +8,17 @@ class Teacher(User):
     
     def __init__(self, first, last):
         super().__init__(first, last)
-        self.knowledge = ["This", "that", "those!"]
+        self.knowledge = ["str is a data type in Python",
+                            "programming is hard, but it's worth it",
+                            "JavaScript async web request",
+                            "Python function call definition",
+                            "object-oriented teacher instance",
+                            "programming computers hacking learning terminal",
+                            "pipenv install pipenv shell",
+                            "pytest -x flag to fail fast",
+                        ]
 
     def teach(self):
-        index = random.randrange(0, len(self.knowledge))
-        return self.knowledge[index]
+        return random.choice(self.knowledge)
+        # index = random.randrange(0, len(self.knowledge))
+        # return self.knowledge[index]
